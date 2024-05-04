0ad2fbfd14094f4436c75789a2f489749cf1ec82
0xyukine <44278060+0xyukine@users.noreply.github.com>
2024-05-04 23:47:03 +0100

    Small bash script to easily get auth code

---
+++ twitch-bot/auth.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+read -p "Code: " clientCo
+read -p "ID: " clientID
+read -p "Secret: " clientSe
+
+
+curl.exe -X POST 'https://id.twitch.tv/oauth2/token' \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -d "client_id=$clientID&client_secret=$clientSe&code=$clientCo&grant_type=authorization_code&redirect_uri=http://localhost:3000"
